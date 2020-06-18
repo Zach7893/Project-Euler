08576795956e8498f4a6f92a1f5de6fba85d20ac
Zach7893 <49004748+Zach7893@users.noreply.github.com>
2020-06-17 22:16:42 -0400

    Add files via upload

---
+++ problem_2/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    //By considering the terms in the Fibonacci sequence whose 
+    //values do not exceed four million, find the sum of the even-valued terms
+    
+    // variable for running total
+    let mut total = 0;
+    let mut numbers = vec![];
+
+    // I'm thinking three variables for the fib sequence
+    let mut x = 1;
+    let mut y = 2;
+    let mut z = 0;
+
+    while z <= 4000000 {
+        z = x + y;
+        numbers.push(z);
+        x = y;
+        y = z;
+    }
+
+    println!("{:?}",numbers);
+}
